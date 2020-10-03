626ac7c220e693be1807112834ff6c0bfc9c2997
Ivan Sokolin <bratislavka21@mail.ru>
2020-10-03 21:30:59 +0300

    Созд.: класс App

---
+++ vendor/ishop/core/App.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace ishop;
+
+
+class App
+{
+    public static $app;
+
+    public function __construct()
+    {
+        $query = trim($_SERVER['QUERY_STRING'], '/');
+        session_start();
+        self::$app = Registry::getInstance();
+        $this->getProperties();
+    }
+
+    protected function getProperties()
+    {
+        $params = require_once CONF . '/params.php';
+        foreach($params as $k => $v) {
+            self::$app->setProperty($k, $v);
+        }
+    }
+}
